c0f1f93834dcb9ea1c5ccfb198090e45d58f3e11
Maverick Tesini <mavericktesini@gmail.com>
2018-07-29 10:07:55 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -10,14 +10,16 @@ class StudentsController < ApplicationController
   end
 
   def edit
+    find_student
   end
 
   def update
+    find_student.update(post_params)
   end
 
   private
 
   def post_params(*args)
-    params.require(:student).permit(*args)
+    params.require(:student)
   end
 end
